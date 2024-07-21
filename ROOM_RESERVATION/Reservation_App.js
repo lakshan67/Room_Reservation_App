dc5384ccfbccc374071a15d069fcb395901ad802
lakshan <lakshanrandika255@gmail.com>
2024-07-21 23:33:08 +0530

    create room reservation simple app

--- ROOM_RESERVATION/Reservation_App.js
+++ ROOM_RESERVATION/Reservation_App.js
@@ -1,15 +1,51 @@
-function addNewReservation() {
-    const nameInput = document.getElementById('nameInput').value
-    
-    if (nameInput === '') {
-        alert('Please fill the task!')
-        return
+const reservations =  []
+
+
+// Function of add each reservation
+const addReservation = (event) => {
+    event.preventDefault()
+
+    const guestName = document.getElementById('guestName').value
+    const roomNumber = document.getElementById('roomNumber').value
+    const checkInDate = document.getElementById('checkInDate').value
+    const checkOutDate = document.getElementById('checkOutDate').value
+
+    const reservation = {
+        guestName,
+        roomNumber,
+        checkInDate,
+        checkOutDate
     }
 
-
-    const addItems = document.createElement(td)
-    addItems.textContent = nameInput
-
-    document.getElementById('taskList').appendChild(addItems)
+    reservations.push(reservation)
+    displayReservations()
+    document.getElementById('reservationForm').reset()
 
 }
+
+// Function of display all reservation
+const displayReservations = () => {
+    const reservationList = document.getElementById('reservationList')
+    reservationList.innerHTML = ''
+
+    reservations.forEach((reservation, index) => {
+        const row = document.createElement('tr')
+        row.innerHTML = `
+        <td>${reservation.guestName}</td>
+        <td>${reservation.roomNumber}</td>
+        <td>${reservation.checkInDate}</td>
+        <td>${reservation.checkOutDate}</td>
+         <td class="btn btn-danger btn-sm" onclick="deleteReservation(${index})">Delete</td>  
+        `
+        
+
+        reservationList.appendChild(row)
+    })
+}
+
+const deleteReservation = (index) => {
+    reservations.splice(index, 1)
+    displayReservations()
+}
+
+document.getElementById('reservationForm').addEventListener('submit', addReservation)
